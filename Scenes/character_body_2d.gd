8f4eddc868f7b625985509d9a4e6c513ccad2d0a
wedekingluxrylan-eng <wedekingluxrylan@gmail.com>
2026-08-05 23:21:19 -0500

    camera and model
    8/5/26: Created a beta player model for testing, with a basic ground. Its quite literally just a staring sim., but im too lazy to make him move rn

---
+++ Scenes/character_body_2d.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_axis("walk_left", "walk_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
